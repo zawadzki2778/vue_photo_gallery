a56460dba23c26090e2ffdbca754c413b2c367df
Maksim Zawadzki <zawadzki2778@gmail.com>
2022-11-02 14:41:00 +0300

    created comp.Navbar and exported into Header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
   <v-app>
-    <!--HEADER-->
-    <v-container>
-      <v-app-bar color="deep-orange" dark>
-        <v-spacer></v-spacer
-        ><!--Создает пустое пространство по вертикали или горизонтали, в данном случае сдвинул кнопки в право -->
-        <v-btn outlined class="mr-5">ГЛАВНАЯ</v-btn
-        ><!-- добавил outlined что бы убрать цвет по дефолту-->
-        <v-btn outlined class="mr-5">ФОТО</v-btn>
-      </v-app-bar>
-    </v-container>
+    <Navbar />
   </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
 export default {
   name: "App",
-  // components: {
-  // },
+  components: { Navbar },
   data: () => ({
     //
   }),
---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <!--HEADER-->
+  <v-container>
+    <v-app-bar color="deep-orange" dark>
+      <v-spacer></v-spacer
+      ><!--Создает пустое пространство по вертикали или горизонтали, в данном случае сдвинул кнопки в право -->
+      <v-btn outlined class="mr-5">ГЛАВНАЯ</v-btn
+      ><!-- добавил outlined что бы убрать цвет по дефолту-->
+      <v-btn outlined class="mr-5">ФОТО</v-btn>
+    </v-app-bar>
+  </v-container>
+</template>
