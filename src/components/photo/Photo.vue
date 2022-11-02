463f51f5723a5c3e488183e4d61c83cec29c05cb
Maksim Zawadzki <zawadzki2778@gmail.com>
2022-11-02 21:57:08 +0300

    extended pseudo photo using props

---
+++ src/components/photo/Photo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-container> {{ photo.title + photo.id}} </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: "Photo",
+
+  props: {
+    photo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,10 +1,31 @@
 <template>
-  <v-container> PHOTOS </v-container>
+  <div>
+    <v-container>
+      <Photo
+        v-for="(photo, id) in photos" 
+        :key="id"
+        :photo="photo"
+      />
+    </v-container>
+  </div>
 </template>
 
 <script>
+import Photo from "@/components/photo/Photo.vue";
 export default {
   name: "PhotosPage",
+  components: { Photo },
+  data() {
+    return {
+      photos: [
+        { id: 1, title: "photo_1" },
+        { id: 2, title: "photo_2" },
+        { id: 3, title: "photo_3" },
+        { id: 4, title: "photo_4" },
+        { id: 5, title: "photo_5" },
+      ],
+    };
+  },
 };
 </script>
 
