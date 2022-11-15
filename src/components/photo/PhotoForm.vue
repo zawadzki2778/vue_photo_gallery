06fe77c460d8d62a4cd586280472bbece7eb3a36
Maksim Zawadzki <zawadzki2778@gmail.com>
2022-11-15 17:22:13 +0300

    wrote the logic for the component

--- src/components/photo/PhotoForm.vue
+++ src/components/photo/PhotoForm.vue
@@ -1,17 +1,35 @@
 <template>
     <v-continer>
         <v-row class="d-flex">
-            <v-text-field />
-            <v-file-input />
-            <v-btn>Добавить фото</v-btn>
+            <v-text-field  v-model="title"/> <!-- связываем input с переменными из data, теперь при каждом изм.инпуту меняются и значения переменных в дате-->
+            <v-file-input v-model='img'/>
+            <v-btn @click="addPhoto">Добавить фото</v-btn>
         </v-row>
     </v-continer>
 </template>
 
 <script>
 export default {
-    name: 'PhotoForm'
-
+    name: 'PhotoForm',
+    data() {
+        return {
+            title: '',
+            img: ''
+        }
+    },
+    methods: {
+        addPhoto() {
+            const reader = new FileReader() // с помощью объекта JS FileReader мы сможеи прочитать фото
+            reader.onload = () => { // слушатель обрабатывает момент когда изображение было прочитано 
+                let photo = {
+                    id: Data.now(), // получаем из текущей даты просто для уникальности
+                    title: this.title,
+                    url: reader.result
+                }
+            }
+            reader.readAsDataURL(this.img)           
+        }
+    }
 }
 </script>
 
